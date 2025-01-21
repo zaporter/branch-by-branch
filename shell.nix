c9dc32b6847731e9055af80e8b7ea3b03fce88c6
zaporter <opensource@zackporter.com>
2025-01-21 13:30:35 -0500

    add shell

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{pkgs ? import <nixpkgs> {}}: let
+  unstableTarball =
+    fetchTarball
+    https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
+  pkgs = import <nixpkgs> {};
+  unstable = import unstableTarball {};
+in
+  pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+      viddy # fancy watch
+    ];
+    shellHook = ''
+    '';
+  }
