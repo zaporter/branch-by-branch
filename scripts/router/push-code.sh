90188efb8194c6655ada9fb09b30428c86de2751
zaporter <opensource@zackporter.com>
2025-01-21 18:42:00 -0500

    fix push code

--- scripts/router/push-code.sh
+++ scripts/router/push-code.sh
@@ -3,10 +3,12 @@
 # Push code locally to the router
 set -euxo pipefail
 
+source ./secrets/router-params.sh
+
 rsync -zv \
     --info=progress2 \
-    -e "ssh -i ./secrets/hetzner" \
+    -e "ssh -i $ROUTER_SSH_KEY" \
     --exclude=.git \
     --exclude=.direnv \
     . \
-    root@/share/repo/branch-by-branch
+    $ROUTER_USER@$ROUTER_IP:/share/repo/branch-by-branch
