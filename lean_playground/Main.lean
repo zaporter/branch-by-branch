a37291700c173898fa5163245e0d40de8de0755c
zaporter <opensource@zackporter.com>
2025-01-23 16:28:44 -0500

    Copy over mathlib data dir

--- lean_playground/Main.lean
+++ lean_playground/Main.lean
@@ -2,10 +2,21 @@ import LeanPlayground
 import Std
 import Init.Data.Nat.Basic
 
+open Nat
 #print Nat
 theorem solv (n:Nat): n+0= n := Nat.add_zero n
 
-example : m + 0 = m := solv m
+theorem succ_pred (n : Nat) : n ≠ 0 → succ (pred n) = n := by
+  intro (hn : n ≠ 0)
+  cases n with
+  | zero => exact absurd rfl (hn : 0 ≠ 0)
+  | succ n => rw [Nat.pred_succ]
+
+example : m + 0 = m := by exact?
+example : 0 + m = m := by exact?
+--#print Real
+
+--theorem mathd_algebra_455 (x : Real) (hO : 2 * (2 * (2 * (2 * x))) = 48) : x = 3 := sorry
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
