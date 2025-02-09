d36ac309dd12cd9b74a5095e18709cab54d6b474
zaporter <opensource@zackporter.com>
2025-02-09 17:19:26 -0500

    tests?

--- lean_corelib/Test.lean
+++ lean_corelib/Test.lean
@@ -1,3 +1,10 @@
 -- This file imports all sub-files in the library. It is created automatically during compilation. Do not edit this manually.
+import Test.AlgebraGroupDefs
+import Test.AlgebraGroupZeroOne
 import Test.DataNatBasic
+import Test.DataNatBinaryRec
 import Test.DataRatDefs
+import Test.DataSubtype
+import Test.ForceDataNatDefs
+import Test.LogicBasic
+import Test.OrderDefsLinearOrder
