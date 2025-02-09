f2c4082d7cbab29c3aa943af3adcc5c6f7ab43c2
zaporter <opensource@zackporter.com>
2025-02-08 23:38:39 -0500

    corelib polish

--- lean_corelib/Test.lean
+++ lean_corelib/Test.lean
@@ -4,4 +4,3 @@ import Corelib
 example : m + 0 = m := by exact?
 example : 0 + m = m := by exact?
 example : ∀ n, n ≠ 0 ∧ n ≠ 1 → Nat.succ (Nat.pred n) = n := by exact?
-example : ∀ n, n ≠ 0 ∧ n ≠ 2 → Nat.succ (Nat.pred n) = n := by exact?
